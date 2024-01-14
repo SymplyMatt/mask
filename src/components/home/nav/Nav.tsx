364b92de43aaef9f7c63ce2fd4a419aee6cc8960
Matt <symplymatt@gmail.com>
2024-01-14 03:13:56 +0100

    sticky nav

--- src/components/home/nav/Nav.tsx
+++ src/components/home/nav/Nav.tsx
@@ -1,16 +1,38 @@
-import React, { useContext } from 'react'
-import mask_logo from './mask_logo.png'
+import React, { useContext, useEffect, useState } from 'react';
+import mask_logo from './mask_logo.png';
 import { Context } from '../../../context/AppContext';
 import TopNav from './TopNav';
 import SearchFilter from './SearchFilter';
+
 const Nav = () => {
-    const { isDarkMode, toggleTheme } = useContext(Context);
+  const { isDarkMode, toggleTheme } = useContext(Context);
+  const [isVisible, setIsVisible] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      setIsVisible(scrollY === 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="flex flex-col w-full px-50">
+    <>
+      {isVisible && <div
+        className={`flex flex-col w-full px-50 transition-opacity duration-300 ${
+          isVisible ? 'opacity-100' : 'opacity-0 pointer-events-none'
+        }`}
+      >
         <TopNav />
         <SearchFilter />
-    </div>
-  )
-}
+      </div>}
+    </>
+  );
+};
 
-export default Nav
+export default Nav;
--- src/components/home/nav/StickyNav.tsx
+++ src/components/home/nav/StickyNav.tsx
@@ -1,20 +1,42 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react';
 import { Context } from '../../../context/AppContext';
 import HamburgerProfile from './HamburgerProfile';
 import Logo from './Logo';
 import StickySearch from './StickySearch';
+
 const StickyNav = () => {
-    const { isDarkMode, toggleTheme } = useContext(Context);
+  const { isDarkMode, toggleTheme } = useContext(Context);
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      setIsVisible(scrollY > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="flex flex-col w-full">
-        <div className='py-20 grid grid-cols-3 w-full items-center  justify-between m-b-20 px-50'>
-            <Logo />
-            <StickySearch />
-            <HamburgerProfile />
+    <>
+      <div
+        className={`flex flex-col w-full fixed top-0 right-0 z-10 bg-white transition-all duration-300 ${
+          isVisible ? 'opacity-100' : 'opacity-0 -translate-y-full'
+        }`}
+      >
+        <div className='py-20 grid grid-cols-3 w-full items-center justify-between m-b-20 px-50'>
+          <Logo />
+          <StickySearch />
+          <HamburgerProfile />
         </div>
         <div className="border-b border-gray-300 w-full"></div>
-    </div>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default StickyNav
+export default StickyNav;
